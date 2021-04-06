059d4a1fd7d283a82021d9a22367ec5b4513b24d
Radon Rosborough <radon.neon@gmail.com>
2021-04-05 21:08:42 -0700

    Stub shallan.el

---
+++ emacs/shallan.el
@@ -0,0 +1,23 @@
+;;; shallan.el --- Music library frontend -*- lexical-binding: t -*-
+
+;; Copyright (C) 2021 Radon Rosborough
+
+;; Author: Radon Rosborough <radon.neon@gmail.com>
+;; Created: 5 Apr 2021
+;; Homepage: https://github.com/raxod502/shallan
+;; Keywords: applications
+;; Package-Requires: ((emacs "26"))
+;; SPDX-License-Identifier: MIT
+;; Version: 0
+
+;;; Commentary:
+
+;; Please see https://github.com/raxod502/shallan for more information.
+
+;;; Code:
+
+
+
+(provide 'shallan)
+
+;;; shallan.el ends here
