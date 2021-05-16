aaa4afcbebf288cc3479a054b7abaf64e13d3541
Radon Rosborough <radon.neon@gmail.com>
2021-05-16 15:35:45 -0700

    Initialize react-native project

---
+++ app/env.sh
@@ -0,0 +1,2 @@
+export ANDROID_HOME="${ANDROID_HOME:-${HOME}/Android/Sdk}"
+export PATH="${PATH}:${ANDROID_HOME}/emulator:${ANDROID_HOME}/tools:${ANDROID_HOME}/tools/bin:${ANDROID_HOME}/platform-tools"
